4ca74136641218e9d17ece8ffe44345e87dc67c7
Steve Mullins <sgmullins@gmail.com>
2019-10-01 13:53:09 -0600

    Add review routes

---
+++ routes/reviews.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+/* GET reviews index /posts/:id/reviews */
+router.get('/', (req, res, next) => {
+    res.send('INDEX /posts/:id/reviews');
+  });
+  
+// /* GET reviews new /posts/:id/reviews/new */
+// router.get('/new', (req, res, next) => {
+//     res.send('NEW /reviews/new');
+//   });
+
+/* GET reviews create /posts/:id/reviews */
+router.post('/', (req, res, next) => {
+    res.send('CREATE /posts/:id/reviews');
+  });
+
+// /* GET reviews show /posts/:id/reviews/review_id */
+// router.get('/:review_id', (req, res, next) => {
+//     res.send('SHOW /posts/:id/reviews_id');
+//   });
+
+ /* GET reviews edit /posts/:id/reviews/:review_id/edit */
+router.get('/:review_id/edit', (req, res, next) => {
+    res.send('EDIT /posts/:id/reviews/:review_id/edit');
+  });
+
+/* PUT reviews update /posts/:id/reviews/:review_id */
+router.put('/:review_id', (req, res, next) => {
+    res.send('UPDATE /posts/:id/reviews/:review_id');
+  });
+
+/* DELETE reviews destroy /posts/:id/reviews/:review_id */
+router.delete('/:review_id', (req, res, next) => {
+    res.send('DELETE /posts/:id/reviews/:review_id');
+  });
+
+module.exports = router;
